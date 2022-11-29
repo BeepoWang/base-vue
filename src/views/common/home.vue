c0a071d8eb18278cbfae1d6fedba4a59580a5db9
BeppoWang <beppowang@gmail.com>
2022-11-29 10:24:28 +0800

    feat: 调整页面与配置

--- src/views/common/home.vue
+++ src/views/common/home.vue
@@ -1,11 +1,22 @@
 <template>
+  <p class="font-bold">
+    <span class="i-mdi-arrow-left"></span>
+    <a @click="jumpPage">User</a>
+  </p>
   <div>home {{ user.userName }}</div>
 </template>
 
 <script lang="ts" setup>
 import useUserStore from '@/store/modules/user'
+import { useRouter } from 'vue-router'
 
 const user = useUserStore()
-
 user.userName = 'ssss'
+
+const router = useRouter()
+const jumpPage = () => {
+  router.push({
+    path: '/user'
+  })
+}
 </script>
--- src/views/user/user.vue
+++ src/views/user/user.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>User</div>
+  <div>
+    <p class="font-bold">
+      <span class="i-mdi-arrow-left"></span>
+      <a @click="jumpPage">Home</a>
+    </p>
+    <p>User</p>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const jumpPage = () => {
+  router.push({
+    path: '/home'
+  })
+}
+</script>
